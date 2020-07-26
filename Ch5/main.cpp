ff48a52496405c7ce5ce3693c89467ce12c523a8
Zack <zwithz1998@gmail.com>
2020-07-26 20:46:01 +0800

    🔫 New Target: Chapter 5

---
+++ Ch5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ch5
+//
+//  Created by zz on 7/26/20.
+//  Copyright © 2020 zwithz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
